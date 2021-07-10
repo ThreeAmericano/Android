d2e725642664f4882ea141a103cd6402a168b2d9
WSP99 <nosod3@naver.com>
2021-07-11 01:46:42 +0900

    add EditTest Test

--- app/src/main/java/com/psw9999/car2smarthome/MainActivity.kt
+++ app/src/main/java/com/psw9999/car2smarthome/MainActivity.kt
@@ -2,11 +2,27 @@ package com.psw9999.car2smarthome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import com.psw9999.car2smarthome.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
+        setContentView(binding.root)
+        binding.TestName.addTextChangedListener {
+            Log.d("EditText","입력된 이름=${it.toString()}")
+        }
+        binding.TestTime.addTextChangedListener {
+            Log.d("EditText","입력된 시간=${it.toString()}")
+        }
+        binding.TestPassword.addTextChangedListener {
+            Log.d("EditText","입력된 비밀번호=${it.toString()}")
+        }
     }
 }
